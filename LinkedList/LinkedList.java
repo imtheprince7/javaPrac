aad55a99f05194bdffd6b4ec378b30fba4cd8987
Prince Kumar <princejnu7@hotmail.com>
2023-01-07 15:44:58 +0530

    new commit

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,44 +1,49 @@
 package LinkedList;
+
 import java.util.Scanner;
+
 public class LinkedList {
-    class Node {
-        int data; 
+    static class Node {
+        int data;
         Node next;
-         Node(int data){
+
+        Node(int data) {
             this.data = data;
-            this.next = null; 
-         }
+            this.next = null;
+        }
     }
     Node head = null;
 
-    public static void insertNode(){
-        Scanner sc = new Scanner (System.in);
-        System.out.println("Enter Element to insert into Node");
-        int data = sc.nextInt();
-        
-
-    }
-    public static void printNode(){
-        Node temp =null;
-    }
-
-
-    public static void main(String[] args) 
-    {
-        Scanner sc = new Scanner (System.in);
-        System.out.println("Enter Option to Perform Linked List Operation:");
-        System.out.print("1. InsertNode 2. PrintNode:\t ");
-        int opt = sc.nextInt();
-        switch(opt)
-        {
-            case 1:
-            insertNode();
-            break;
-            case 2:
-            printNode();
-            break;
-
-        }
+    public void insertAtBegin() {
+        int data, n;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Enter Element to insert into Node");
+            data = sc.nextInt();
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = newNode;
+            } else {
+                newNode.next = head;
+                head = newNode;
+            }
+            System.out.println("Do you Wanna Continue: Press  1 or Press ANY_KEY");
+            n = sc.nextInt();
+        } while (n == 1);
         sc.close();
+    }
+
+    public void printNode() {
+        Node temp = head;
+        while (temp.next != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.insertAtBegin();
+        ll.printNode(); 
     }
 }

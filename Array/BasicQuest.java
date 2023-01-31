b92261a7029f2fd0f177778b8fd0fb58c01feec3
Prince <princejnu7@hotmail.com>
2023-01-31 11:40:20 +0530

    new Question

---
+++ Array/BasicQuest.java
@@ -0,0 +1,18 @@
+public class BasicQuest {
+    public static void main(String[] args) {
+        int num = 123456;
+        System.out.println(findPalindorm(num));
+    }
+    static boolean findPalindorm(int num){
+        int n=num, rem=0, rev =0;
+        while(n>0){
+            rem = n%10;
+            n/= n;
+            rev = (rev*10)+rem;
+        }
+        if(rev==num)
+            return true;
+        else
+            return false;
+    }
+}

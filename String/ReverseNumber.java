1694be5736a0d4a19dfecf9652ea9c7bdba2ffb6
Prince <princejnu7@hotmail.com>
2023-02-02 17:11:35 +0530

    new

--- String/ReverseNumber.java
+++ String/ReverseNumber.java
@@ -3,7 +3,9 @@ package String;
 public class ReverseNumber {
     public static void main(String[] args) {
         String str ="prince Kumar";
-        reverseNum(str);
+         System.out.println(reverseNum(str));
+         System.out.println(reverseString(str));
+
     }
     public static String reverseNum(String str){
         StringBuilder sb = new StringBuilder();
@@ -12,5 +14,14 @@ public class ReverseNumber {
             sb.append(str.charAt(i));
         }
         return  sb.toString();
+    }
+
+    public static String reveString(String str){
+        String ans ="";
+        for(int i=str.length(); i>=0; i++){
+            ans += str.charAt(i);
+        }
+        return ans;
+        
     }
 }

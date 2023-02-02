4c65f52c57e9ec3e24fb799e83d648bbdf2cd3cc
Prince <princejnu7@hotmail.com>
2023-02-02 17:24:08 +0530

    new ques

--- String/ReverseNumber.java
+++ String/ReverseNumber.java
@@ -5,6 +5,7 @@ public class ReverseNumber {
         String str ="prince Kumar";
          System.out.println(reverseNum(str));
          System.out.println(reverseString(str));
+         System.out.println(removeVowel(str));
 
     }
     public static String reverseNum(String str){
@@ -16,12 +17,28 @@ public class ReverseNumber {
         return  sb.toString();
     }
 
-    public static String reveString(String str){
+    public static String reverseString(String str){
         String ans ="";
         for(int i=str.length(); i>=0; i++){
             ans += str.charAt(i);
         }
         return ans;
-        
     }
+
+    static String removeVowel(String str){
+        StringBuffer sb = new StringBuffer();
+       for(int i=0; i< str.length(); i++){
+            if(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i' || str.charAt(i)=='o' || str.charAt(i)=='u'){
+                continue;
+            }
+            else{
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+
+
+
 }

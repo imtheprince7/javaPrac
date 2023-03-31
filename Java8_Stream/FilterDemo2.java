8c961334ce9b9a1d1fb165eea42e5cfc0f2a46bc
imtheprince7 <princejnu7@hotmail.com>
2023-03-31 12:16:25 +0530

    Stream_Method

--- Java8_Stream/FilterDemo2.java
+++ Java8_Stream/FilterDemo2.java
@@ -9,21 +9,21 @@ public class FilterDemo2 {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Prince","Aashish","Chanchal","Aashi Singh","Ankita Singh","Kirti Kumari","Priya Singh","Shubhangi");
         
-        // To print Only NAME which size >=6 &&  size <=8 SEVERAL methods:
+// To print Only NAME which size >=6 &&  size <=8 SEVERAL methods:
     
-        // 1. Normal Method ForEach Method()
+// 1. Normal Method ForEach Method()
         for (String str : names) {
             if(str.length()>=6 &&str.length()<=8){
                 System.out.print(str+" ");
             }            
         }
-        
         System.out.println(" ");        
-        // By JAVA_8 =======  STREAM filer() method & Storing in new List
+
+
+// By JAVA_8 =======  STREAM filer() method & Storing in new List
         
         names.stream().filter(n -> n.length() >=6 && n.length()<=11 ).forEach(s->System.out.print(s+" "));
         System.out.println("");
-        
         names.stream().filter(n -> n.length() >=6 && n.length()<=14).forEach(System.out::print);
                
 
--- Java8_Stream/MapDemo1.java
+++ Java8_Stream/MapDemo1.java
@@ -9,11 +9,11 @@ public class MapDemo1 {
     public static void main(String[] args) {
         List<String> vehicles = Arrays.asList("thar","swift","kia","fortuner","scarpio","bolero","pajero","bmw","ninja");
         
-        // To convert all the List object in UpperCase.
+// To convert all the List object in UpperCase.
         vehicles.stream().map(u->u.toUpperCase()).collect(Collectors.toList()).forEach(s-> System.out.print(s+" "));
         
 
-        // TO count the length of every object and print them
+// TO count the length of every object and print them
         System.out.println("TO count the length of every object and print them");
         vehicles.stream().map(st -> st.length()).collect(Collectors.toList()).forEach(System.out::println);
 

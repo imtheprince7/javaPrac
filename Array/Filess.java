ab111f0978762e6ee595cd855a458a10b2f30fd2
imtheprince7 <princeks3577@gmail.com>
2025-01-14 15:35:10 +0530

    14 Jan

---
+++ Array/Filess.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.Math;
+
+class Filess{
+
+    public static void main(String[] args) {
+        int a_val = Math.abs(-1);
+        int [] arr = {1,4,2,7,3,9,7,6};
+        Arrays.sort(arr);
+
+        System.out.println("Value of a:"+" "+a_val);
+    }
+}
+
